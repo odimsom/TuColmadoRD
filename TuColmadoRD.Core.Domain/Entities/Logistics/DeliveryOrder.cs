250beb7dee0f9466282ef81d2511212b9db0ae18
Francisco Daniel Castro Borrome <borrome941@gmail.com>
2026-03-14 19:46:02 -0400

    Remove unused using directive from logistics and sales files
    Removed TuColmadoRD.Domain.Core.ValueObjects using directive from DeliveryOrder.cs, Sale.cs, and Shift.cs to clean up redundant references and improve code clarity.

--- TuColmadoRD.Core.Domain/Entities/Logistics/DeliveryOrder.cs
+++ TuColmadoRD.Core.Domain/Entities/Logistics/DeliveryOrder.cs
@@ -2,7 +2,6 @@
 using TuColmadoRD.Core.Domain.Base.Result;
 using TuColmadoRD.Core.Domain.Enums.Logistics;
 using TuColmadoRD.Core.Domain.ValueObjects;
-using TuColmadoRD.Domain.Core.ValueObjects;
 
 namespace TuColmadoRD.Core.Domain.Entities.Logistics
 {
