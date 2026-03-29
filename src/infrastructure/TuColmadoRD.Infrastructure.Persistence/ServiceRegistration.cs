e69684ab64f4d6294b5ce0d243bd1eeab51390ef
Francisco Daniel Castro Borrome <borrome941@gmail.com>
2026-03-29 15:59:14 -0400

    fix(inventory): ensure product stock quantity is not null in response refactor(infrastructure): remove unused security interface from service registration

--- src/infrastructure/TuColmadoRD.Infrastructure.Persistence/ServiceRegistration.cs
+++ src/infrastructure/TuColmadoRD.Infrastructure.Persistence/ServiceRegistration.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using TuColmadoRD.Core.Application.Interfaces.Security;
 using TuColmadoRD.Core.Domain.Interfaces.Repositories.Audit;
 using TuColmadoRD.Core.Domain.Interfaces.Repositories.Base;
 using TuColmadoRD.Core.Domain.Interfaces.Repositories.Customers;
